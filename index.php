984def90c76d4f5b08f4d24cb99df2176c67ec31
Kat Truitt <ktruitt@mail.greenriver.edu>
2019-04-20 15:14:19 -0400

    Fixed routing from home to personal information page

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 //turn on error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
@@ -10,17 +9,32 @@ require_once('vendor/autoload.php');
 //create an instance of the Base class
 $f3 = Base::instance();
 
+
 //Define a default root
-$f3->route('GET /', function()
+$f3->route('GET/', function()
 {
+    //echo'<h1>HELLO WORLD</h1>';
     $view = new Template();
-    echo $view->render
-    ('views/home.html');
-}
-);
+    echo $view->render('views/home.html');
+});
 
-
+$f3->route('POST /profile', function()
+{
+    //echo'<h1>HELLO WORLD</h1>';
+    $view = new Template();
+    echo $view->render('views/personal-information.html');
+});
+$f3->route('POST /profile-page', function()
+{
+    //echo'<h1>HELLO WORLD</h1>';
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+/*$f3->route('POST /profile-page', function()
+{
+    //echo'<h1>HELLO WORLD</h1>';
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});*/
 //Run Fat-free
 $f3->run();
-
-
