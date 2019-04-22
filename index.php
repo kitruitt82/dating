67f56e41793691d0f75e76e5894b568e8652809e
Kat Truitt <ktruitt@mail.greenriver.edu>
2019-04-22 04:42:13 -0400

    Added session variables to index page Still working on interests array and getting values for radion buttons and check boxes

--- index.php
+++ index.php
@@ -11,12 +11,20 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 
+
 //Define a default root
 $f3->route('GET /', function()
 {
     //echo'<h1>HELLO WORLD</h1>';
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+$f3->route('GET /home', function()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+
 });
 
 $f3->route('GET /personal', function()
@@ -81,6 +89,11 @@ $f3->set('states',array('Alabama','Alaska',
 
 $f3->route('POST /personal', function()
 {
+    $_SESSION['fname'] = $_POST['fname'] ;
+    $_SESSION['lname'] = $_POST['lname'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['phone'] = $_POST['phone'];
     //echo'<h1>HELLO WORLD</h1>';
     $view = new Template();
     echo $view->render('views/form2.html');
@@ -96,9 +109,27 @@ $f3->set('outdoor',array('hiking','biking','swimming','collecting','walking','cl
 
 $f3->route('POST /profile', function()
 {
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['genderSeeking'] = $_POST['genderSeeking'];
     //echo'<h1>HELLO WORLD</h1>';
     $view = new Template();
     echo $view->render('views/interests.html');
+});
+
+$f3->route('POST /interests',function()
+{
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+$f3->route('GET /confirmation', function()
+{
+    print_r($_SESSION['personal']);
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
+
 });
 //Run Fat-free
 $f3->run();
