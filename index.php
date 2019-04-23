1a5b6ec94e85cd7a21d4b3f55ac406ab9c3d004f
Kat Truitt <ktruitt@mail.greenriver.edu>
2019-04-22 20:40:44 -0400

    Fixed array function in index for select menu in form2.html

--- index.php
+++ index.php
@@ -29,13 +29,12 @@ $f3->route('GET /home', function()
 
 $f3->route('GET /personal', function()
 {
-    $_SESSION['personal'] = $_POST['fname'];
     //echo'<h1>HELLO WORLD</h1>';
     $view = new Template();
     echo $view->render('views/form1.html');
 });
 //array for state select
-$f3->set('states',array('Alabama','Alaska',
+$states=array('Alabama','Alaska',
             'Arizona',
             'Arkansas',
             'California',
@@ -85,10 +84,12 @@ $f3->set('states',array('Alabama','Alaska',
             'West Virginia',
             'Wisconsin',
             'Wyoming'
-        ));
+        );
+$f3->set('states',$states);
 
 $f3->route('POST /personal', function()
 {
+    //include('model/validation.php');
     $_SESSION['fname'] = $_POST['fname'] ;
     $_SESSION['lname'] = $_POST['lname'];
     $_SESSION['gender'] = $_POST['gender'];
@@ -110,7 +111,7 @@ $f3->set('outdoor',array('hiking','biking','swimming','collecting','walking','cl
 $f3->route('POST /profile', function()
 {
     $_SESSION['email'] = $_POST['email'];
-    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['states'] = $_POST['states'];
     $_SESSION['genderSeeking'] = $_POST['genderSeeking'];
     //echo'<h1>HELLO WORLD</h1>';
     $view = new Template();
@@ -119,13 +120,24 @@ $f3->route('POST /profile', function()
 
 $f3->route('POST /interests',function()
 {
+
+    $activities=[];
+    $_SESSION['interests'] = $_POST['interests'];
+
+    if(isset($_SESSION['interests']))
+    {
+        $activities= $_SESSION['interests'];
+    }
+
+    $activity=implode(", " , $activities);
+    $_SESSION['interests']=$activity;
+
     $view = new Template();
     echo $view->render('views/summary.html');
 });
 
 $f3->route('GET /confirmation', function()
 {
-    print_r($_SESSION['personal']);
 
     $view = new Template();
     echo $view->render('views/summary.html');
